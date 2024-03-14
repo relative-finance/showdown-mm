00138ae9de98bab632ab90441a45c3538af41fe1
Filip Pinjuh <filip.pinjuh@gmail.com>
2024-03-14 14:16:54 +0100

    websocket WIP

--- pkg/server/api/ticket.go
+++ pkg/server/api/ticket.go
@@ -2,18 +2,38 @@ package api
 
 import (
 	"context"
+	"time"
 
 	"mmf/pkg/services"
 	"mmf/wires"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func RegisterTicket(router *gin.Engine, ctx context.Context) {
 	tickets := router.Group("/tickets")
 	{
 		tickets.POST("/submit", submitTicket)
 	}
+
+	router.GET("/ws", func(c *gin.Context) {
+		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			return
+		}
+		defer conn.Close()
+		for {
+			conn.WriteMessage(websocket.TextMessage, []byte("Hello, WebSocket!"))
+			time.Sleep(time.Second)
+		}
+	})
+
 }
 
 func submitTicket(c *gin.Context) {
